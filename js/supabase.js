8815a7eb08934c354aeca81a877ef4020cd6759a
EmersonMoraes <emersoncarlosr.moraes@gmail.com>
2026-03-13 23:59:50 -0300

    Supabase connection for user login

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://coinqixwtvytooidlpdn.supabase.co";
+const supabaseKey = "sb_publishable_Pwo4nzG_08j_rg9E1DIplg_yLPB01Yy";
+
+window.supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
